4005c16949d128beb256decb432c95fb5f06ad25
BlazingCosmos <cbruder2@elon.edu>
2024-04-23 01:28:02 -0400

    Added proposed new code to WordFamily method, now using a map

--- Assignment6/Game.cpp
+++ Assignment6/Game.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <string>
 #include <iostream>
+#include <map>
 
 //CONSTRUCTOR
 Game::Game(int wordLength, bool showWordCount, list<string> potWords) {
@@ -63,17 +64,41 @@ void Game::WordFamily(char guess, list<string> words) {
     list<string> boiledWords; //tracks words that contain the letter
     list<string> emptyWords; //tracks words that don't contain the letter
     list<string> returnWords; //The list of strings we'll return
+    map <string, list<string>> wordFamilies; //improved version of WordFam, will track all words with same "index find string"
+    string indexKey; //key that will be used in the dictionary
     for (int i = 0; i < wordLength+1; i++) {
         WordFam[i] = 0;
     }
     for (string word : words) {
-        int Found = word.find(guess);
-        if (Found != string::npos) {
-            boiledWords.push_back(word);
-            WordFam[Found] += 1;
-        } else {
-            emptyWords.push_back(word);
-            WordFam[wordLength] += 1;
+        // int Found = word.find(guess);
+        // if (Found != string::npos) {
+        //     boiledWords.push_back(word);
+        //     WordFam[Found] += 1;
+        // } else {
+        //     emptyWords.push_back(word);
+        //     WordFam[wordLength] += 1;
+        // }
+        for (int i  = 0; i < word.length()-1; i++) { //use i for indexing
+            //resets the word family indexes at each new word
+            if (i == 0) {
+                indexKey.clear();
+            }
+            //marks where the letter shows up in the word
+            if (word[i] == guess) {
+                indexKey.append(to_string(i));
+            }
+            //okay this next part is kinda messy and can probably be improved later
+            //at last char, places word in map
+            if (i + 1 >= word.length()-1) {
+                if (indexKey.empty()) {
+                    emptyWords.push_back(word);
+                    continue;
+                }
+                //that should be fine for words that are empty, right?
+                wordFamilies[indexKey] = word;
+                //not sure how to add words to the list of strings
+            }
+            //i'll add potential code for the deciding part down there later, but here's my general idea
         }
     }
     cout << "Num words: " << boiledWords.size() << endl;
